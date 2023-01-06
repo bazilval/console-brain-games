11f9adc09b2716fbc56ee704557f05fa52d61e62
Valentin Bazilevich <bazilvalentin@gmail.com>
2023-01-06 17:43:27 +0300

    Fix App.java and build.gradle

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,7 +1,6 @@
-package src;
-
+package hexlet.code;
 class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
